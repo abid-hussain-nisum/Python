05a4ff881559f16fa98d3871c4fb22b524b56053
abid-hussain-nisum <mahussain@nisum.com>
2024-11-12 18:28:03 +0530

    .

--- 2.Strings-and-ConditionalStatements/3.StringFunctions.py
+++ 2.Strings-and-ConditionalStatements/3.StringFunctions.py
@@ -5,5 +5,5 @@ print(str.capitalize()) #capitalizes the first character
 print(str.replace("how","HOW")) #replaces the old occurence with new
 print(str) #original string is not changed
 print(str.find("a"))  #returns the 1st index of the 1st occurrer
-a = str.count("how")
+a = str.count("how")  #counts the occurence of substr in string
 print(a)
